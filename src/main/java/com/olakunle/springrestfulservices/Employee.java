8556bf519a3e68595e03f8d04371f013301c9d91
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-01-06 04:39:23 +0100

    showing the App supports changes, by adding fristName and lastName properties

--- src/main/java/com/olakunle/springrestfulservices/Employee.java
+++ src/main/java/com/olakunle/springrestfulservices/Employee.java
@@ -16,13 +16,19 @@ public class Employee {
     private @Id
     @GeneratedValue Long id;
     private String name;
+    private String firstName;
+    private String lastName;
+
     private String role;
+
+
 
    public Employee() {}
 
-    Employee(String name, String role) {
+    Employee(String firstName,String lastName, String role) {
 
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -30,9 +36,14 @@ public class Employee {
         return this.id;
     }
 
+
+
+
     public String getName() {
-        return this.name;
+        return this.firstName + " " + this.lastName;
     }
+
+
 
     public String getRole() {
         return this.role;
@@ -43,7 +54,9 @@ public class Employee {
     }
 
     public void setName(String name) {
-        this.name = name;
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     public void setRole(String role) {
@@ -58,17 +71,18 @@ public class Employee {
         if (!(o instanceof Employee))
             return false;
         Employee employee = (Employee) o;
-        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-                && Objects.equals(this.role, employee.role);
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+                && Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+                + '\'' + ", role='" + this.role + '\'' + '}';
     }
 }
