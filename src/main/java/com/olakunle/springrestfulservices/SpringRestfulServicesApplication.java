be3c5f8139100f8845523cf06d5614fed99e38e5
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-01-06 03:37:47 +0100

    Basics of spring web service, without adding restful features

---
+++ src/main/java/com/olakunle/springrestfulservices/SpringRestfulServicesApplication.java
@@ -0,0 +1,11 @@
+package com.olakunle.springrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulServicesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/olakunle/springrestfulservices/SpringRestfulServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olakunle.springrestfulservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
